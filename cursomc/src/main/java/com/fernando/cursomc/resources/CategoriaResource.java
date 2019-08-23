a6aebe1dcbb94d7b233bc63c91953b96a43ba241
fernando.oliveira <fernando.oliveira@cerc.inf.br>
2019-08-23 13:03:13 -0300

    testando a primeira classe de dominio

--- cursomc/src/main/java/com/fernando/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/fernando/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.fernando.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fernando.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,8 +15,16 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest Funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+	
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);	
+		
+		return lista;
 	}
 
 }
