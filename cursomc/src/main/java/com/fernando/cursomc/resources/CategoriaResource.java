5d19cdd929020496435461dfae5a9dd2ebccb6ca
fernandoguide <fernandoguide2014@gmail.com>
2019-08-26 13:39:57 -0300

    Inserindo categorias

--- cursomc/src/main/java/com/fernando/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/fernando/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.fernando.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fernando.cursomc.domain.Categoria;
 import com.fernando.cursomc.services.CategoriaService;
@@ -22,6 +26,13 @@ public class CategoriaResource {
 		
 		Categoria obj = service.find(id);	
 		return ResponseEntity.ok().body(obj) ;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- cursomc/src/main/java/com/fernando/cursomc/services/CategoriaService.java
+++ cursomc/src/main/java/com/fernando/cursomc/services/CategoriaService.java
@@ -25,5 +25,9 @@ public class CategoriaService {
 				 "Objeto nao encontrado! Id: " + id
 				 + " Tipo: "+ Categoria.class.getName()));
 		}
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 
 }
