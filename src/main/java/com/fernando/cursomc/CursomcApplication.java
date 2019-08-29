45f50e5003fb2b115b3588d10974fac2b2aaabe9
fernandoguide <fernandoguide2014@gmail.com>
2019-08-29 11:47:10 -0300

    Salvando o primeiro arquivo no Amazon s3

--- src/main/java/com/fernando/cursomc/CursomcApplication.java
+++ src/main/java/com/fernando/cursomc/CursomcApplication.java
@@ -1,16 +1,26 @@
 package com.fernando.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fernando.cursomc.services.S3Service;
+
 @SpringBootApplication
-public class CursomcApplication  {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
-
-	public void run(String... args) throws Exception {		
+	
+	@Override
+	public void run(String... args) throws Exception {	
+//		s3Service.uploadFile("/home/fernando.oliveira/Desktop/39173306.jpeg");
 	}
 
 }
+
